d75288f6090bc96b60f663384d104fc834782503
graykr <graykr@users.noreply.github.com>
2020-04-06 17:39:30 -0500

    moved the output file to another directory
    to avoid cluttering the project code list

--- DOM_parse_final.py
+++ DOM_parse_final.py
@@ -4,8 +4,8 @@ import os.path
 
 list_of_ids = []
 
-#test list
-file_path_to_ids = "sample_list_of_ids.txt"
+#test list to use
+file_path_to_ids = "short_sample_ids.txt"
 
 #full list (comment out this variable assignment if only testing)
 #file_path_to_ids = "list_of_eads.txt"
@@ -30,17 +30,17 @@ for i in list_of_ids:
     #print(create_path(ead_id))
     dom = xml.dom.minidom.parse(create_path(ead_id))
     file = dom.documentElement
-    #system id
-    list_sys_ids.append(ead_id)
+
     #unititle
     unittitle = dom.getElementsByTagName("unittitle")
     list_titles.append(unittitle)
 
 #create the file if it doesn't exist, append if it does
+path_titles_file = os.path.join("output","ead_titles.txt")
 try:
-    file = open(r"ead_titles.txt","x")
+    file = open(path_titles_file,"x")
 except:
-    file = open(r"ead_titles.txt","a")
+    file = open(path_titles_file,"a")
 
 for item in list_titles:
     sent = item[0].firstChild.data.strip()
