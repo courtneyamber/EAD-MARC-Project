a564e503466444a31b3817dac373afa321390094
graykr <graykr@users.noreply.github.com>
2020-04-06 17:15:41 -0500

    additional edits
    paths to work independent of operating system; commented out print statements for debugging; started bulding list also for system ids to prep for csv export

--- DOM_parse_final.py
+++ DOM_parse_final.py
@@ -1,5 +1,6 @@
 from xml.dom.minidom import parse
 import xml.dom.minidom
+import os.path
 
 list_of_ids = []
 
@@ -12,21 +13,26 @@ file_path_to_ids = "sample_list_of_ids.txt"
 with open(file_path_to_ids, "r") as inputfile:
     for line in inputfile:
         list_of_ids.append(line.strip().split(','))
-print (list_of_ids)
+
+#print (list_of_ids)
 
 def create_path(id):
-    base_path = "EAD Files\ead_file_"
+    base_path = os.path.join("EAD Files","ead_file_")
     end_path = ".xml"
     pathdir = base_path + id + end_path
     return pathdir
 
+list_sys_ids = []
 list_titles = []
 
 for i in list_of_ids:
     ead_id = i[0]
-    print(create_path(ead_id))
+    #print(create_path(ead_id))
     dom = xml.dom.minidom.parse(create_path(ead_id))
     file = dom.documentElement
+    #system id
+    list_sys_ids.append(ead_id)
+    #unititle
     unittitle = dom.getElementsByTagName("unittitle")
     list_titles.append(unittitle)
 
@@ -37,7 +43,8 @@ except:
     file = open(r"ead_titles.txt","a")
 
 for item in list_titles:
-    sent = item[0].firstChild.data
-    print(sent, sep='')
+    sent = item[0].firstChild.data.strip()
+    sent+="\n"
+    #print(sent, sep='')
     file.write(sent)
 file.close()
